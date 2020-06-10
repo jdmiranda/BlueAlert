fa46e4e3635f766b11190f9dbd5f6eb0e6ca7667
jeremy <jeremy.d.miranda@gmail.com>
2020-06-10 01:48:30 -0400

    testing

--- scripts/site.js
+++ scripts/site.js
@@ -2,9 +2,24 @@ var village;
 (function () {
   "use strict";
 
+  console.log('test');
   L.mapbox.accessToken = 'pk.eyJ1IjoiYW1icmlhc2hpciIsImEiOiJjaWZ0MXAybDcwZ3I2dHRseWI3NjAyMTZ2In0.eD7uxIRAY9ifI6ecnkiu-g';
   var map = new L.mapbox.Map();
   map.addControl(new L.mapbox.NavigationControl());
   json = $.getJSON('http://raw.githubusercontent.comjdmiranda/bluealerts/testinglinks/blue.json')
+  var markers = new L.MarkerClusterGroup();
 
-}
+
+  function processJsonGroups(data) {
+    data.forEach(addMarker);
+    map.addLayer(markers);
+  };
+
+  function addMarker(m) {
+    const coordinates = m.geometry.coordinates;
+    const props = m.properties;
+    console.log(m);
+  };
+
+
+});
