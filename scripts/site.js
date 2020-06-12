008787d14ba1b5076421483b00a158868a66eef0
jeremy <jeremy.d.miranda@gmail.com>
2020-06-12 11:21:05 -0400

    marker beautification pass

--- scripts/site.js
+++ scripts/site.js
@@ -31,12 +31,12 @@ var bluealert;
 
   //Camera
 
-      function addSelfMarker(){
-        console.log('creating self marker here');
-        var marker = new mapboxgl.Marker()
-            .setLngLat([longitude,latitude])
-            .addTo(map);
-      };
+  function addSelfMarker() {
+    console.log('creating self marker here');
+    var marker = new mapboxgl.Marker()
+      .setLngLat([longitude, latitude])
+      .addTo(map);
+  };
 
   document.getElementById('camera').addEventListener('click', function() {
     fly(map, longitude, latitude);
@@ -73,74 +73,132 @@ var bluealert;
       1
     ]);
 
-        var geojson = [{
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-77.031952, 38.913184]
-        },
-        properties: {
-          icon: {
-            iconUrl: 'https://www.mapbox.com/mapbox.js/assets/images/astronaut1.png',
-            iconSize: [50, 50], // size of the icon
-            iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -25], // point from which the popup should open relative to the iconAnchor
-            className: 'dot'
-          }
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-94.636230, 46.392410]
-        },
-        properties: {
-          name: 'georgefloydmurder',
-          icon: {
-            iconUrl: 'https://www.mapbox.com/mapbox.js/assets/images/astronaut2.png',
-            iconSize: [50, 50], // size of the icon
-            iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -25], // point from which the popup should open relative to the iconAnchor
-            className: 'dot'
-          }
-        }
+    map.addSource('blueAlert', {
+      type: 'geojson',
+      // Point to GeoJSON data. This example visualizes all M1.0+ blueAlert
+      // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+      data: 'https://raw.githubusercontent.com/jdmiranda/bluealerts/master/blue.geojson',
+      cluster: true,
+      clusterMaxZoom: 14, // Max zoom to cluster points on
+      clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
+
+    map.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: 'blueAlert',
+      filter: ['has', 'point_count'],
+      paint: {
+        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        'circle-color': [
+          'step',
+          ['get', 'point_count'],
+          '#51bbd6',
+          100,
+          '#f1f075',
+          750,
+          '#f28cb1'
+        ],
+        'circle-radius': [
+          'step',
+          ['get', 'point_count'],
+          20,
+          100,
+          30,
+          750,
+          40
+        ]
       }
-    ];
+    });
 
-geojson.forEach(addMarker);
+    map.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: 'blueAlert',
+      filter: ['has', 'point_count'],
+      layout: {
+        'text-field': '{point_count_abbreviated}',
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12
+      }
+    });
 
+    map.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: 'blueAlert',
+      filter: ['!', ['has', 'point_count']],
+      paint: {
+        'circle-color': '#11b4da',
+        'circle-radius': 4,
+        'circle-stroke-width': 1,
+        'circle-stroke-color': '#fff'
+      }
+    });
 
-    function addMarker(m){
-      console.log(m);
-      var marker = new mapboxgl.Marker()
-          .setLngLat(m.geometry.coordinates)
-          .addTo(map);
-    };
+    // inspect a cluster on click
+    map.on('click', 'clusters', function(e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ['clusters']
+      });
+      var clusterId = features[0].properties.cluster_id;
+      map.getSource('blueAlert').getClusterExpansionZoom(
+        clusterId,
+        function(err, zoom) {
+          if (err) return;
 
-    // map.addSource('points', {
-    //   'type':'geojson',
-    //   'data': geojson
-    // });
-    // map.addLayer('points');
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+          });
+        }
+      );
+    });
 
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on('click', 'unclustered-point', function(e) {
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var mag = e.features[0].properties.mag;
+      var tsunami;
 
+      if (e.features[0].properties.tsunami === 1) {
+        tsunami = 'yes';
+      } else {
+        tsunami = 'no';
+      }
 
-    //   console.log("adding popup");
-    //
-    //   new mapboxgl.Popup()
-    //     .setLngLat(coordinates)
-    //     .setHTML(
-    //       '<iframe width="560" height=315" src="' + stream_id + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-    //     )
-    //     .addTo(map);
-    // });
+      // Ensure that if the map is zoomed out such that
+      // multiple copies of the feature are visible, the
+      // popup appears over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      //sample used
+      //'<iframe src="//player.vimeo.com/video/106112939" width="380" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="http://vimeo.com/106112939"><h2>How Simplicity Will Save GIS</h2><p>Vladimir Agafonkin</a> from <a href="http://vimeo.com/foss4g">FOSS4G</a> on <a href="https://vimeo.com">Vimeo</a>.</p>'
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(
+          '<iframe src="' + e.features[0].properties.stream_id + '" width="380" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' +
+          '<p><a href="' + e.features[0].properties.stream_id + '"><h2>How Simplicity Will Save GIS</h2><p>Vladimir Agafonkin</a> from <a href="http://vimeo.com/foss4g">FOSS4G</a> on ' +
+          '<a href="https://vimeo.com">Vimeo</a>.</p>'
+        )
+        .addTo(map);
+    });
 
     map.on('mouseenter', 'clusters', function() {
       map.getCanvas().style.cursor = 'pointer';
     });
     map.on('mouseleave', 'clusters', function() {
-      map.getCanvas().style.cursor = 'pointer';
+      map.getCanvas().style.cursor = '';
     });
   });
 
